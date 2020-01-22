e48822aaa07ab04a90c0e2deef16dd4498bc29c7
Danilo Augusto <dasf_cm@terra.com.br>
2020-01-22 00:30:20 -0300

    MenuBar, MenuLinks, PostItem and SocialLinks components

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/klasrak',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/klasrak',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com/klasrak',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/danilo_augusto93',
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://unsplash.com/@dasfcm',
+  },
+];
+
+export default links;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
+import PostItem from '../components/PostItem';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      background="black"
+      category="Misc"
+      date="21 de janeiro de 2020"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
   </Layout>
 );
 
